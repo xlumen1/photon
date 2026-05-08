24a5ff0ec44815b5326472154deb426724920925
Gus Stoermer <gstoermer1@gmail.com>
2026-05-08 19:35:54 -0400

    Remove addressing mode cycles

--- src/cpu/addr.rs
+++ src/cpu/addr.rs
@@ -4,7 +4,6 @@ use super::CPU;
 // Immediate Addressing
 
 pub(super) fn addr_imm_acc(s: &mut CPU) -> Operand {
-    s.inst_cycles(1);
     let value = if s.acc_size() == 1 {
         let v = s.read8(((s.pb as u32) << 16) | s.pc as u32);
         s.pc = s.pc.wrapping_add(1);
@@ -18,7 +17,6 @@ pub(super) fn addr_imm_acc(s: &mut CPU) -> Operand {
 }
 
 pub(super) fn addr_imm_idx(s: &mut CPU) -> Operand {
-    s.inst_cycles(1);
     let value = if s.idx_size() == 1 {
         let v = s.read8(((s.pb as u32) << 16) | s.pc as u32);
         s.pc = s.pc.wrapping_add(1);
@@ -32,14 +30,12 @@ pub(super) fn addr_imm_idx(s: &mut CPU) -> Operand {
 }
 
 pub(super) fn addr_imm_byte(s: &mut CPU) -> Operand {
-    s.inst_cycles(1);
     let value = s.read8(((s.pb as u32) << 16) | s.pc as u32);
     s.pc = s.pc.wrapping_add(1);
     Operand::Immediate(value as u16)
 }
 
 pub(super) fn addr_imm_word(s: &mut CPU) -> Operand {
-    s.inst_cycles(1);
     let value = s.fetch16();
     Operand::Immediate(value as u16)
 }
@@ -47,20 +43,17 @@ pub(super) fn addr_imm_word(s: &mut CPU) -> Operand {
 // Absolute Addressing
 
 pub(super) fn addr_abs(s: &mut CPU) -> Operand {
-    s.inst_cycles(2);
     let addr = s.fetch16();
     Operand::Address(((s.db as u32) << 16) | addr as u32)
 }
 
 pub(super) fn addr_abs_x(s: &mut CPU) -> Operand {
-    s.inst_cycles(2);
     let base = s.fetch16();
     let addr = base.wrapping_add(s.x);
     Operand::Address(((s.db as u32) << 16) | addr as u32)
 }
 
 pub(super) fn addr_abs_y(s: &mut CPU) -> Operand {
-    s.inst_cycles(2);
     let base = s.fetch16();
     let addr = base.wrapping_add(s.y);
     Operand::Address(((s.db as u32) << 16) | addr as u32)
@@ -69,21 +62,18 @@ pub(super) fn addr_abs_y(s: &mut CPU) -> Operand {
 // Direct Page Addressing
 
 pub(super) fn addr_dp(s: &mut CPU) -> Operand {
-    s.inst_cycles(2);
     let offset = s.fetch8();
     let addr = s.dp.wrapping_add(offset as u16) as u32;
     Operand::Address(addr)
 }
 
 pub(super) fn addr_dp_x(s: &mut CPU) -> Operand {
-    s.inst_cycles(3);
     let offset = s.fetch8();
     let addr = s.dp.wrapping_add(offset as u16).wrapping_add(s.x) as u32;
     Operand::Address(addr)
 }
 
 pub(super) fn addr_dp_y(s: &mut CPU) -> Operand {
-    s.inst_cycles(3);
     let offset = s.fetch8();
     let addr = s.dp.wrapping_add(offset as u16).wrapping_add(s.y) as u32;
     Operand::Address(addr)
@@ -92,7 +82,6 @@ pub(super) fn addr_dp_y(s: &mut CPU) -> Operand {
 // Long Addressing
 
 pub(super) fn addr_long(s: &mut CPU) -> Operand {
-    s.inst_cycles(3);
     let lo = s.fetch8() as u32;
     let md = s.fetch8() as u32;
     let hi = s.fetch8() as u32;
@@ -100,7 +89,6 @@ pub(super) fn addr_long(s: &mut CPU) -> Operand {
 }
 
 pub(super) fn addr_long_x(s: &mut CPU) -> Operand {
-    s.inst_cycles(3);
     let lo = s.fetch8() as u32;
     let md = s.fetch8() as u32;
     let hi = s.fetch8() as u32;
@@ -108,7 +96,6 @@ pub(super) fn addr_long_x(s: &mut CPU) -> Operand {
 }
 
 pub(super) fn addr_long_y(s: &mut CPU) -> Operand {
-    s.inst_cycles(3);
     let lo = s.fetch8() as u32;
     let md = s.fetch8() as u32;
     let hi = s.fetch8() as u32;
@@ -118,7 +105,6 @@ pub(super) fn addr_long_y(s: &mut CPU) -> Operand {
 // Indirect Addressing
 
 pub(super) fn addr_ind_abs(s: &mut CPU) -> Operand {
-    s.inst_cycles(5);
     let ptr = s.fetch16();
     let lo = s.read8(((s.db as u32) << 16) | ptr as u32) as u16;
     let hi = s.read8(((s.db as u32) << 16) | (ptr.wrapping_add(1)) as u32) as u16;
@@ -127,7 +113,6 @@ pub(super) fn addr_ind_abs(s: &mut CPU) -> Operand {
 }
 
 pub(super) fn addr_dp_ind(s: &mut CPU) -> Operand {
-    s.inst_cycles(5); // TODO: Fix cycle count to have full accuracy
     let zp = s.fetch8();
     let dp_mask = if s.emulation { 0xFF } else { 0xFFFF };
     
@@ -143,7 +128,6 @@ pub(super) fn addr_dp_ind(s: &mut CPU) -> Operand {
 }
 
 pub(super) fn addr_dp_ind_x(s: &mut CPU) -> Operand {
-    s.inst_cycles(6);
     let zp = s.fetch8().wrapping_add(s.x as u8);
     let lo = s.read8(s.dp.wrapping_add(zp as u16) as u32);
     let hi = s.read8(s.dp.wrapping_add(zp as u16 + 1) as u32);
@@ -152,7 +136,6 @@ pub(super) fn addr_dp_ind_x(s: &mut CPU) -> Operand {
 }
 
 pub(super) fn addr_dp_ind_y(s: &mut CPU) -> Operand {
-    s.inst_cycles(5);
     let zp = s.fetch8();
     let lo = s.read8(s.dp.wrapping_add(zp as u16) as u32);
     let hi = s.read8(s.dp.wrapping_add(zp as u16 + 1) as u32);
@@ -161,7 +144,6 @@ pub(super) fn addr_dp_ind_y(s: &mut CPU) -> Operand {
 }
 
 pub(super) fn addr_ind_abs_x(s: &mut CPU) -> Operand {
-    s.inst_cycles(6);
     let base  = s.fetch16();
     let ptr = base.wrapping_add(s.x);
     let addr = s.read16(ptr as u32);
@@ -169,7 +151,6 @@ pub(super) fn addr_ind_abs_x(s: &mut CPU) -> Operand {
 }
 /*
 pub(super) fn addr_dp_long_ind(s: &mut CPU) -> Operand {
-    s.inst_cycles(6);
     let zp = s.fetch8();
     let base = s.dp.wrapping_add(zp as u16) as u32;
     let lo = s.read8(base) as u32;
@@ -179,7 +160,6 @@ pub(super) fn addr_dp_long_ind(s: &mut CPU) -> Operand {
 }
 
 pub(super) fn addr_dp_long_ind_y(s: &mut CPU) -> Operand {
-    s.inst_cycles(6); // Check
     let zp = s.fetch8();
     let base = s.dp.wrapping_add(zp as u16) as u32;
     let lo = s.read8(base) as u32;
@@ -192,13 +172,11 @@ pub(super) fn addr_dp_long_ind_y(s: &mut CPU) -> Operand {
 // Stack Relative Addressing
 
 pub(super) fn addr_sr(s: &mut CPU) -> Operand {
-    s.inst_cycles(3);
     let offset = s.fetch8();
     Operand::Address(s.sp.wrapping_add(offset as u16) as u32)
 }
 
 pub(super) fn addr_sr_ind_y(s: &mut CPU) -> Operand {
-    s.inst_cycles(6);
     let offset = s.fetch8();
     let lo = s.read8(s.sp.wrapping_add(offset as u16) as u32);
     let hi = s.read8(s.sp.wrapping_add(offset as u16 + 1) as u32);
@@ -209,14 +187,12 @@ pub(super) fn addr_sr_ind_y(s: &mut CPU) -> Operand {
 // Relative Addressing
 
 pub(super) fn addr_rel(s: &mut CPU) -> Operand {
-    s.inst_cycles(2);
     let offset = s.fetch8() as i8;
     let target = s.pc.wrapping_add(offset as u16);
     Operand::Relative(target as i16)
 }
 
 pub(super) fn addr_rel_long(s: &mut CPU) -> Operand {
-    s.inst_cycles(3);
     let offset = s.fetch16() as i16;
     let target = s.pc.wrapping_add(offset as u16);
     Operand::Relative(target as i16)
@@ -225,7 +201,6 @@ pub(super) fn addr_rel_long(s: &mut CPU) -> Operand {
 // Special
 
 pub(super) fn addr_blk_mov(s: &mut CPU) -> Operand {
-    s.inst_cycles(2);
     let src_bank = s.fetch8();
     let dst_bank = s.fetch8();
     Operand::Block { src_bank, dst_bank }
